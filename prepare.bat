d38c4ba7812b79d0b015daa0ab1466b1928c0c23
Abdurrahim Cakar <abdurrahim.cakar@gmail.com>
2023-01-10 00:45:55 +0100

    Correct the script

--- prepare.bat
+++ prepare.bat
@@ -1,3 +1,4 @@
+@echo off
 setlocal
 
 set VCPKG_DEFAULT_TRIPLET=x64-windows
@@ -23,7 +24,33 @@ popd
 set CMAKE_TOOLCHAIN_FILE=%~dp0third-party\vcpkg\scripts\buildsystems\vcpkg.cmake
 
 md %~dp0build
-pushd %~dp0build
 compact /c "%~dp0build" /i /Q
-cmake ..  -DCMAKE_TOOLCHAIN_FILE="%CMAKE_TOOLCHAIN_FILE%" && cmake --build .
+pushd build
+call :CALL_CMAKE .. -DCMAKE_TOOLCHAIN_FILE="%CMAKE_TOOLCHAIN_FILE%" && cmake --build .
 popd
+goto :EOF
+
+:CALL_CMAKE
+setlocal
+
+where cmake && (
+  call cmake %* && exit /B 0 || exit /B 1
+) || (
+  echo CMake is not in path. Searching CMake installed from VCpkg
+  pushd %~dp0third-party\vcpkg\downloads\tools
+  for /F %%F in ('dir /B /S cmake.exe') do (
+    popd
+    pushd %%F\..
+    goto :FOUND_CMAKE_ADD_PATH
+:FOUND_CMAKE_ADD_PATH_RETURN
+    popd
+    call cmake %*
+    goto :EOF
+  )
+  popd
+  exit /B 1
+)
+goto :EOF
+:FOUND_CMAKE_ADD_PATH
+set PATH=%PATH%;%CD%
+goto :FOUND_CMAKE_ADD_PATH_RETURN
